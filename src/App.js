cdf89ccecc563dab5f74918712c9ebc41505b4a3
unknown <ycc525@nyu.edu>
2019-12-10 21:58:13 +0800

    final

--- src/App.js
+++ src/App.js
@@ -1,204 +1,145 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import generate from '@babel/generator';
-import domtoimage from 'dom-to-image';
 import date from 'date-and-time';
-import { Upload, Icon, Modal } from 'antd';
+import { statement } from '@babel/template';
 
 
-
-let topchoices=[
-  {url:"https://bit.ly/2pSTXUT"},
-  {url:"https://bit.ly/2KXZFMw"},
-  {url:"https://bit.ly/2KVNyzN"}
+let topchoices = [
+  { url: "https://bit.ly/2pSTXUT" },
+  // { url: "https://bit.ly/2KXZFMw" },
+  // { url: "https://bit.ly/2KVNyzN" }
 ];
-let bottomchoices=[
-  {url:"https://bit.ly/2Dmsn5v"},
-  {url:"https://bit.ly/33iOLr6"},
-  {url:"https://bit.ly/2R0Af4Z"}
+let bottomchoices = [
+  { url: "https://bit.ly/2Dmsn5v" },
+  // { url: "https://bit.ly/33iOLr6" },
+  // { url: "https://bit.ly/2R0Af4Z" }
 ];
-let shoeschoices=[
-  {url:"https://bit.ly/2XNtUv4"},
-  {url:"https://bit.ly/2KTF5wQ"},
-  {url:"https://bit.ly/2DmdnEK"}
+let shoeschoices = [
+  { url: "https://bit.ly/2XNtUv4" },
+  // { url: "https://bit.ly/2KTF5wQ" },
+  // { url: "https://bit.ly/2DmdnEK" }
 ]
-let bagchoices=[
-  {url:"https://bit.ly/37w1rhz"},
-  {url:"https://bit.ly/33n2I7m"},
-  {url:"https://bit.ly/2OJJ1kW"}
+let bagchoices = [
+  { url: "https://bit.ly/37w1rhz" },
+  // { url: "https://bit.ly/33n2I7m" },
+  // { url: "https://bit.ly/2OJJ1kW" }
 ]
 
 const now = new Date();
 
-let style1={
-  visibility:'hidden'
+let style1 = {
+  visibility: 'hidden'
 }
-let style2={
-  visibility:'visible'
+let style2 = {
+  visibility: 'visible'
 }
 
-function App(){
+function App() {
   return (
     <div className="App">
       <div id="anchor"></div>
-      
+
       <p id="title">What should be my outfit of {date.format(now, 'MMM DD')} ?</p>
-      <button id="generate" onClick={generator} >Generate!!</button> 
-      <div id="outfit">
-      <img id="bottom" src={bottomchoices[0].url}/>
-      <img id="top" src={topchoices[0].url}/>
-      <img id="bag" src={bagchoices[0].url}/>
-      <img id="shoes" src={shoeschoices[0].url}/> 
+      <button id="generate" onClick={generator} >Generate!!</button>
+      <div id="outfit" style={style1}>
+        <img id="bottom" src={bottomchoices[0].url} />
+        <img id="top" src={topchoices[0].url} />
+        <img id="bag" src={bagchoices[0].url} />
+        <img id="shoes" src={shoeschoices[0].url} />
       </div>
       <div className="wardrobe">
         <ul id="list" style={style1}>
-          <a href="#tc"><li>Top</li></a>
-          <a href="#bc"><li>Bottom</li></a>
-          <a href="#sc"><li>Shoes</li></a>
-          <a href="#bgc"><li>Bag</li></a>
-        </ul> 
-      <button id="wardrobe" onMouseOver={list} onMouseOut={list2}>Wardrobe</button>    
+          <a href="#Top"><li>Top</li></a>
+          <a href="#Bottom"><li>Bottom</li></a>
+          <a href="#Shoes"><li>Shoes</li></a>
+          <a href="#Bag"><li>Bag</li></a>
+        </ul>
+        <button id="wardrobe" onMouseOver={list} onMouseOut={list2}>Wardrobe</button>
       </div>
       <div id="choices">
-        <div id="tc">
-        <p>Top Choices</p>
-        <br></br>
-        {topchoices.map(topselection)}
-        <form onChange={handleChange}>
-        <input type="text" multiple="false" placeholder="Your Cloth URL" ></input>
-        <input type="submit" value="Submit"></input>
-        </form>
-        </div>
-        <div id="bc">
-        <p>Bottom Choices</p>
-        <br></br>
-        {bottomchoices.map(bottomselection)}
-        <form onChange={handleChange2}>
-        <input type="text" multiple="false" placeholder="Your Cloth URL" ></input>
-        <input type="submit" value="Submit"></input>
-        </form>
-        </div>
-        <div id="sc">
-        <p>Shoes Choices</p>
-        <br></br>
-        {shoeschoices.map(shoesselection)}
-        <form onChange={handleChange3}>
-        <input type="text" multiple="false" placeholder="Your Cloth URL" ></input>
-        <input type="submit" value="Submit"></input>
-        </form>
-        </div>
-        <div id="bgc">
-        <p>Bag Choices</p>
-        <br></br>
-        {bagchoices.map(bagselection)}
-        <form onChange={handleChange4}>
-        <input type="text" multiple="false" placeholder="Your Cloth URL" ></input>
-        <input type="submit" value="Submit"></input>
-        </form>
-        </div>
-       
-       </div>
+        <Choices title="Top" choices={topchoices} />
+        <Choices title="Bottom" choices={bottomchoices} />
+        <Choices title="Shoes" choices={shoeschoices} />
+        <Choices title="Bag" choices={bagchoices} />
+      </div>
       <a href="#anchor"><button id="return">Going back UP!</button></a>
     </div>
-    
-    );}
-  
+
+  );
+}
+
+
+
 //random generate
 
-let length0 = topchoices.length;
-let length1 = topchoices.length;
-let length2 = topchoices.length;
-let length3 = topchoices.length;
 
-function generator(){
-  document.getElementById('top').src=topchoices[Math.floor(Math.random()* length0 )].url;
-  document.getElementById('bottom').src=bottomchoices[Math.floor(Math.random()*length1 )].url;
-  document.getElementById('bag').src=bagchoices[Math.floor(Math.random()* length2)].url;
-  document.getElementById('shoes').src=shoeschoices[Math.floor(Math.random()* length3)].url;
+
+function generator() {
+let length0 = topchoices.length;
+let length1 = bottomchoices.length;
+let length2 = bagchoices.length;
+let length3 = shoeschoices.length;
+  document.getElementById('outfit').style = { style2 };
+  document.getElementById('top').src = topchoices[Math.floor(Math.random() * length0)].url;
+  document.getElementById('bottom').src = bottomchoices[Math.floor(Math.random() * length1)].url;
+  document.getElementById('bag').src = bagchoices[Math.floor(Math.random() * length2)].url;
+  document.getElementById('shoes').src = shoeschoices[Math.floor(Math.random() * length3)].url;
 }
 
 //showing list
 
-function list(){
-  document.getElementById('list').style={style2};
+function list() {
+  document.getElementById('list').style = { style2 };
 }
-function list2(){
-  document.getElementById('list').style={style1};
+function list2() {
+  document.getElementById('list').style = { style1 };
 };
-//uploading images
 
-
-function handleChange(event){
-  let add = {url: event.target.value};
-  topchoices.push(add);
-  console.log(add);
-  console.log(topchoices);
-  console.log(length0);
-}
-function handleChange2(event){
-  bottomchoices.push(event.target.value);
-}
-function handleChange3(event){
-  shoeschoices.push(event.target.value);
-}
-function handleChange4(event){
-  bagchoices.url.push(event.target.value);
-}
 //display images
 
+function Choices(props) {
+  
+  let [choices, setChoices]= useState(props.choices);
+  let [texts, setTexts] =useState()
+  function handleChange() {
+    let newChoices= [...choices , {url: texts}]
+    console.info('form', texts);
+    let add = { url: texts };
+    props.choices.push(add);
+    setChoices(newChoices);
+   };
+function textChange(params) {
+  setTexts(params.target.value)
+}
+  return (
+    <div id={props.title}>
+      <p>{props.title} Choices</p>
+      {choices.map(selection)}
+      <form onSubmit={handleChange}>
+        <input onChange={textChange} type="text" multiple="false" placeholder="Look it up on the website!" ></input>
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>);
+}
 
-function topselection(data){
+function selection(data) {
   return (
     <div>
-    <TOP src={data.url}/>
+      <CHOICES src={data.url} />
     </div>
   );
 }
 
 
-function bottomselection(data){
+function CHOICES(props) {
   return (
-    <div>
-    <BOTTOM src={data.url}/>
-    </div>
-  );
-}
-function shoesselection(data){
-  return (
-    <div>
-    <SHOES src={data.url}/>
-    </div>
-  );
-}
-function bagselection(data){
-  return (
-    <div>
-    <BAG src={data.url}/>
-    </div>
-  );
-}
-function TOP(props){ 
-  return(
-    <img className="display" src={props.src}/>
+    <img className="display" src={props.src} />
   );
 
- 
+
 }
 
-function BOTTOM(props){
-  return(
-    <img className="display" src={props.src}/>
-  )
-}
-function SHOES(props){
-  return(
-    <img className="display" src={props.src}/>
-  )
-}
-function BAG(props){
-  return(
-    <img className="display" src={props.src}/>
-  )
-}
+
 export default App;
 
